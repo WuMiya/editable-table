c3845c0fdb20146b251884c290eb735d0a51c57e
miyatruii <miya.wu@truii.com>
2018-10-11 11:36:17 +1000

    Updated context menu, fixed its position

--- src/app/newtable/newtable.component.ts
+++ src/app/newtable/newtable.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input
+} from '@angular/core';
 
 @Component({
   selector: 'app-newtable',
@@ -9,52 +13,72 @@ export class NewtableComponent implements OnInit {
   // Sortable table
   private sorted: boolean = false;
   private editField: string = "";
-  private width: number=1000;
-  private height: number=480;
-  private textAlignment: string="Left";
-  private headerFontSize: number=16;
-  private tableFontSize: number=14;
-  private fontWeight: string="Normal";
-  private headerAlignment: string="Left";
+  private width: number = 1000;
+  private height: number = 480;
+  private textAlignment: string = "Left";
+  private headerFontSize: number = 16;
+  private tableFontSize: number = 14;
+  private fontWeight: string = "Normal";
+  private headerAlignment: string = "Left";
+  contextmenu = false;
+  contextmenuX = 0;
+  contextmenuY = 0;
+  nRightClicks = 0;
   // rotateDegree: string="";
   // thHeight: string="";
   // downloadabl:boolean = false;
   // translateDegree:string = "";
- 
+
   // for color setting
-  color: string="#ffecee";
-  alternateColor: string="#fff";
-  headerBGColor: string="#fff";
-  headerTxtColor: string="#222";
-  tableTxtColor: string="#333";
-  pageColor: string="#fff";
+  color: string = "#ffecee";
+  alternateColor: string = "#fff";
+  headerBGColor: string = "#fff";
+  headerTxtColor: string = "#222";
+  tableTxtColor: string = "#333";
+  pageColor: string = "#fff";
   recents: string[] = [this.color];
 
-  cpPresetColors: any[] = ["#ea4256", "#ffa500", "#ffc966", "#f67d3b", "#43e5fd", "#20a5e6", "#1F917A", "#8B572A", 
-    "#000000","#4A4A4A", "#ffffff", "#9B9B9B"];
+  cpPresetColors: any[] = ["#ea4256", "#ffa500", "#ffc966", "#f67d3b", "#43e5fd", "#20a5e6", "#1F917A", "#8B572A",
+    "#000000", "#4A4A4A", "#ffffff", "#9B9B9B"
+  ];
 
-  options = [
-    { name: "Left", value: "left" },
-    { name: "Center", value: "center" },
-    { name: "Right", value: "right" }
+  options = [{
+      name: "Left",
+      value: "left"
+    },
+    {
+      name: "Center",
+      value: "center"
+    },
+    {
+      name: "Right",
+      value: "right"
+    }
   ]
 
   // font-weight options
-  FWoptions = [
-    { name: "Normal", value: "normal" },
-    { name: "Lighter", value: "lighter" },
-    { name: "Bold", value: "bold" }
+  FWoptions = [{
+      name: "Normal",
+      value: "normal"
+    },
+    {
+      name: "Lighter",
+      value: "lighter"
+    },
+    {
+      name: "Bold",
+      value: "bold"
+    }
   ]
 
   @Input() data;
-  @Input() headers=[];
+  @Input() headers = [];
 
   constructor() {
-    
+
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   // sortable table
   sortBy(header: any): void {
@@ -62,10 +86,10 @@ export class NewtableComponent implements OnInit {
 
     this.data.sort((a: any, b: any) => {
       if (a[by] < b[by]) {
-          return this.sorted ? 1 : -1;
+        return this.sorted ? 1 : -1;
       }
       if (a[by] > b[by]) {
-          return this.sorted ? -1 : 1;
+        return this.sorted ? -1 : 1;
       }
       return 0;
     });
@@ -86,7 +110,7 @@ export class NewtableComponent implements OnInit {
   // edit width of the display section
   updateWidth($event) {
     this.width = parseInt($event.target.value, 10);
-    if(this.width > 1600) {
+    if (this.width > 1600) {
       this.width = 1600;
     }
   }
@@ -94,19 +118,19 @@ export class NewtableComponent implements OnInit {
   // edit height of the display section
   updateHeight($event) {
     this.height = parseInt($event.target.value, 10);
-    if(this.height > 2600){
+    if (this.height > 2600) {
       this.height = 2600;
     }
   }
 
   // handle text alignment
-  textAlignmentChange (event: any) {
+  textAlignmentChange(event: any) {
     this.textAlignment = event.target.value;
     return this.textAlignment;
   }
 
   // handle header alignment
-  headerAlignmentChange (event: any) {
+  headerAlignmentChange(event: any) {
     this.headerAlignment = event.target.value;
     return this.headerAlignment;
   }
@@ -114,7 +138,7 @@ export class NewtableComponent implements OnInit {
   // handle header font size
   updateHeaderFontSize($event) {
     this.headerFontSize = parseInt($event.target.value, 10);
-    if(this.headerFontSize > 78){
+    if (this.headerFontSize > 78) {
       this.headerFontSize = 78;
     }
   }
@@ -122,13 +146,13 @@ export class NewtableComponent implements OnInit {
   // handle table font size
   updateTableFontSize($event) {
     this.tableFontSize = parseInt($event.target.value, 10);
-    if(this.tableFontSize > 78){
+    if (this.tableFontSize > 78) {
       this.tableFontSize = 78;
     }
   }
 
   // handle table font weight
-  fontWeightChange (event: any) {
+  fontWeightChange(event: any) {
     this.fontWeight = event.target.value;
     return this.fontWeight;
   }
@@ -144,11 +168,16 @@ export class NewtableComponent implements OnInit {
   //     this.translateDegree = "0%, 0%";
   //   }
   // }
-  nRightClicks = 0;
-  rightClick: boolean = false;
-  onRightClick() {
-    this.rightClick = true;
+
+  onRightClick($event) {
+    this.contextmenuX = $event.clientX
+    this.contextmenuY = $event.clientY
+    this.contextmenu = true;
     this.nRightClicks++;
-    return false;
   }
+
+  disableContextMenu() {
+    this.contextmenu = false;
+  }
+
 }
